588ac43ba9157807de653699840c5e7076ec7fd4
kgw0513 <kkggww0513@naver.com>
2025-11-06 01:43:48 +0900

    make Calendar

--- Assets/Scripts/Calendar/CalendarManager.cs
+++ Assets/Scripts/Calendar/CalendarManager.cs
@@ -1,16 +1,50 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CalendarManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    List<Message> CalendarList = new List<Message>();
+
+    public GameObject CalendarPanel,CalendarListPanel, PopupMessage;
+
     void Start()
     {
-        
+        CalendarPanel.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PopupMessageToCalendar("문자 테스트 중!!! / text testing!!!");
+        }
     }
+
+    public void UpLoadCalendar(List<string> texts)
+    {
+
+    }
+
+    public void PopupMessageToCalendar(string text)
+    {
+        Message newMessage = new Message();
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(PopupMessage, CalendarListPanel.transform);
+
+        CalendarList.Add(newMessage);
+    }
+
+    public void ToggleWindow()
+    {
+        CalendarPanel.SetActive(!CalendarPanel.activeSelf);
+    }
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
 }
