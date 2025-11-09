6d09a41aa38f98b305ac851c7177d4a40b4edf97
kgw0513 <kkggww0513@naver.com>
2025-11-09 12:42:21 +0900

    Rollback

--- Assets/Scripts/Calendar/CalendarManager.cs
+++ Assets/Scripts/Calendar/CalendarManager.cs
@@ -6,48 +6,45 @@ using UnityEngine.UI;
 public class CalendarManager : MonoBehaviour
 {
     [SerializeField]
-    List<CalendarPopUpUI> CalendarList = new List<CalendarPopUpUI>();
+    List<Message> CalendarList = new List<Message>();
 
     public GameObject CalendarPanel,CalendarListPanel, PopupMessage;
 
-    public InputField MessageInputField, TimeInputField;
     void Start()
     {
         CalendarPanel.SetActive(false);
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            UpLoadSchedule();
-            //PopupMessageToCalendar("문자 테스트 중!!! / text testing!!!", "1234-05-13 오전 1:23:45");
+            PopupMessageToCalendar("문자 테스트 중!!! / text testing!!!");
         }
     }
-    public void UpLoadSchedule()
-    {
-        if (MessageInputField.text == "" || TimeInputField.text == "") return;
-        PopupMessageToCalendar(MessageInputField.text, TimeInputField.text);
-        MessageInputField.text = ""; TimeInputField.text = "";
-    }
-    public void PopupMessageToCalendar(string text,string time)
-    {
-        GameObject newMesh = Instantiate(PopupMessage, CalendarListPanel.transform);
-        CalendarPopUpUI newCalendarPopUpUI = newMesh.GetComponent<CalendarPopUpUI>();
-        newCalendarPopUpUI.SetUpPopUI(text,time);
-        newCalendarPopUpUI.OnDeleteRequested += HandlePopUpDelete;
-        CalendarList.Add(newCalendarPopUpUI);
 
+    public void UpLoadCalendar(List<string> texts)
+    {
+
+    }
+
+    public void PopupMessageToCalendar(string text)
+    {
+        Message newMessage = new Message();
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(PopupMessage, CalendarListPanel.transform);
+
+        CalendarList.Add(newMessage);
     }
 
     public void ToggleWindow()
     {
         CalendarPanel.SetActive(!CalendarPanel.activeSelf);
     }
+}
 
-    private void HandlePopUpDelete(CalendarPopUpUI slotToDelete)
-    {
-        CalendarList.Remove(slotToDelete);
-
-        Destroy(slotToDelete.gameObject);
-    }
+[System.Serializable]
+public class Message
+{
+    public string text;
 }
